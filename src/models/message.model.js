0e3948c8a702677f85dd2f3d19ab290d46e1af58
AnisurRahman06046 <anisurrahman06046@gmail.com>
2025-11-09 22:29:12 +0600

    updated

--- src/models/message.model.js
+++ src/models/message.model.js
@@ -1,13 +1,8 @@
 import mongoose from "mongoose";
 
-const fileSchema = new mongoose.Schema(
-  {
-    url: { type: String, required: true },
-    type: { type: String, required: true }, // 'image' or 'raw'
-    mimetype: { type: String, required: true },
-    originalName: { type: String, required: true },
-  },
-  { timestamps: true }
-);
+const messageSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
 
-export default mongoose.model("File", fileSchema);
+export default mongoose.model("Message", messageSchema);
